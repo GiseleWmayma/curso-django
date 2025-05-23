657cae46f0eb6092435738fd58bfe6e88aba3e69
Gisele Wmayma Pinheiro dos Santos <giselewmayma@gmail.com>
2025-05-23 14:59:23 -0400

    alteracoes diversas

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from recipes.views import home
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
-
+# recipes:recipe
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home), #Home
+    path('', views.home, name="home"), #Home
+    path('recipes/category/<int:category_id>/', views.category, name="category"), 
+    path('recipes/<int:id>/', views.recipe, name="recipe"), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
