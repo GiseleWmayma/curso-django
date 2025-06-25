0b76a0cf0d60944c58f811c585643b535fc7d362
Gisele Wmayma Pinheiro dos Santos <giselewmayma@gmail.com>
2025-06-25 16:16:26 -0400

    teste url de todas as views

--- recipes/tests.py
+++ recipes/tests.py
@@ -6,3 +6,11 @@ class RecipeURLsTest(TestCase):
         home_url = reverse('recipes:home') #o que eu to testando
         self.assertEqual(home_url, '/') #se url é igual a barra
         
+    def test_recipe_category_url_is_correct(self):
+        home_url = reverse('recipes:category', kwargs={'category_id': 1}) #testando com argumentos: em ordem> args=(1,),kwargs= manda um dicionario
+        self.assertEqual(home_url, '/recipes/category/1/') #se url é igual a barra
+        
+    def test_recipe_detail_url_is_correct(self):
+        home_url = reverse('recipes:recipe', kwargs={'id': 1}) #testando com argumentos: em ordem> args=(1,),kwargs= manda um dicionario
+        self.assertEqual(home_url, '/recipes/1/') #se url é igual a barra
+        
