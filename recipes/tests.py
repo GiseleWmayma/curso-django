b28470f3a9adb4cdfadb6ed327dc97769ad8019f
Gisele Wmayma Pinheiro dos Santos <giselewmayma@gmail.com>
2025-06-25 15:59:34 -0400

    teste url home

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
 
-class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        variavel = 'gisele'
-        print('Olá Mundo')
-        assert 1 == 1, 'Um é igual a um'
+class RecipeURLsTest(TestCase):   
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home') #o que eu to testando
+        self.assertEqual(home_url, '/') #se url é igual a barra
+        
